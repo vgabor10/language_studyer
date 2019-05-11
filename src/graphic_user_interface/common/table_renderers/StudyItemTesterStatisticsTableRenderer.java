3bcc930d59153c13a1e8d9b1725fc253df9b72e7
vgabor10 <vgabor10@gmail.com>
2019-05-11 16:13:03 +0200

    format indantation

--- src/graphic_user_interface/common/table_renderers/StudyItemTesterStatisticsTableRenderer.java
+++ src/graphic_user_interface/common/table_renderers/StudyItemTesterStatisticsTableRenderer.java
@@ -9,13 +9,13 @@ import common.GeneralFunctions;
 
 public class StudyItemTesterStatisticsTableRenderer implements TableCellRenderer {
     
-    public static final DefaultTableCellRenderer DEFAULT_RENDERER = new DefaultTableCellRenderer();
+	public static final DefaultTableCellRenderer DEFAULT_RENDERER = new DefaultTableCellRenderer();
 
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value,
 	    boolean isSelected, boolean hasFocus, int row, int column) {
             
-	    Component c = DEFAULT_RENDERER.getTableCellRendererComponent(
+		Component c = DEFAULT_RENDERER.getTableCellRendererComponent(
 			table, value, isSelected, hasFocus, row, column);
 		
 		String rarAfter 
