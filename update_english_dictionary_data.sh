51eee0662348ec6d6b40075cd4ebbff066c788e1
Gabor Varga <vgabor10@gmail.com>
2020-01-12 12:00:57 +0100

    add script for easier english data update to repo

---
+++ update_english_dictionary_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git add data/english_data/dictionary_data/words.txt
+git add data/english_data/dictionary_data/card_indexes_to_category_indexes.txt
+git commit -m "update english data"
+git push
