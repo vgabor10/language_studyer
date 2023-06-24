25b87206c8ec0763a471d6676221004c5961088c
Gabor Varga <vgabor10@gmail.com>
2023-06-24 14:42:19 +0200

    check existence of file/directory in clean_repo.sh

--- clean_repo.sh
+++ clean_repo.sh
@@ -2,7 +2,16 @@
 script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$script_path"
 
-rm -r Java
-rm log_files/log_file.txt
+if [ -d  Java ]
+then
+   rm -r Java
+   echo "folder deleted: language_studier/Java"
+fi
+
+if [ -f  log_files/log_file.txt ]
+then
+   rm log_files/log_file.txt
+   echo "file deleted: language_studier/log_files/log_file.txt"
+fi
 
 #data/settings_data/* is not cleaned
